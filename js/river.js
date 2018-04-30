afff3ffdb337c5d14529f165290c9d90d43ab821
BARAODASILVA <BARAODASILVA@intechS1.fr>
2018-04-30 11:37:58 +0200

    Modification fichier

---
+++ js/river.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// RIVER
+
+var contenuRiver = fs.readFileSync("./json/river.json", "UTF");
+var river = JSON.parse(contenuRiver);
+
+var distributionRiver = function(){
+
+	var i;
+
+	for(i = 0;i < river.length; i++){
+		
+		
+	}
+}
