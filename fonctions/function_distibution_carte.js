4719b739b226eac237d9bad0c6da4cb98e0143f4
Nahel Lachgar <Nahel@MacBook-Pro-de-Nahel-2.local>
2018-05-02 12:03:34 +0200

    a

--- fonctions/function_distibution_carte.js
+++ fonctions/function_distibution_carte.js
@@ -8,19 +8,18 @@
 // EN TETE
 
 
-var fs = require("fs");
-var cartes = [];
+var cartes;
 var fs = require("fs"); 
 var cartes;
 var river;
-var contenu;
+var contenu = [];
 var i;
 
 contenu = fs.readFileSync("../json/cartes.json", "UTF-8");
 cartes = JSON.parse(contenu);
 
 river = [];
-cartes = [];
+cartes = {};
 
 // CORPS
 
@@ -29,10 +28,10 @@ for (river = 0 ; river < 5; river++) {
 	do {
 
 		cartes[i] = Math.floor(Math.random() * 52) + 1;
+		console.log(cartes[i]);
+		cartes[i].dispo = "false";
 		i++;
-		console.log(cartes);
-		cartes[i].dispo = false;
-	} while (cartes[i].dispo = false);
+	} while (cartes[i].dispo === "false");
 }
 
 contenu = JSON.stringify(cartes);
