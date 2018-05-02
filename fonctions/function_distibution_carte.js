8c632b1642db1bf5193230fdff072addd8fb7fe6
RODRIGUEZ <RODRIGUEZ@intechS1.fr>
2018-05-02 14:33:41 +0200

    modif distribution cartes

--- fonctions/function_distibution_carte.js
+++ fonctions/function_distibution_carte.js
@@ -12,7 +12,7 @@ var cartes;
 var fs = require("fs"); 
 var cartes;
 var river;
-var contenu = [];
+var contenu;
 var i;
 
 contenu = fs.readFileSync("../json/cartes.json", "UTF-8");
@@ -23,16 +23,15 @@ cartes = {};
 
 // CORPS
 
-for (river = 0 ; river < 5; river++) {
+//for (river = 0 ; river < 5; river++) {
 
 	do {
 
 		cartes[i] = Math.floor(Math.random() * 52) + 1;
-		console.log(cartes[i]);
-		cartes[i].dispo = "false";
+		cartes[i].dispo = false;
 		i++;
-	} while (cartes[i].dispo === "false");
-}
 
+	} while (cartes[i].dispo === false);
+}
 contenu = JSON.stringify(cartes);
 fs.writeFileSync("../json/cartes.json", contenu, "UTF-8");
