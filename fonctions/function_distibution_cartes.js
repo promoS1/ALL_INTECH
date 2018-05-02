807c26f947ff5e9966e225437893202b56f32a7f
RODRIGUEZ <rodriguez@intechinfo.fr>
2018-05-02 18:41:07 +0200

    a

---
+++ fonctions/function_distibution_cartes.js
@@ -0,0 +1,46 @@
+//=========================================================================
+// Traitement de "function_distribution_carte"
+// Auteur : ALL_INTECH
+// Version : 29/04/2018
+//=========================================================================
+"use strict";
+
+// EN TETE
+
+var fs = require("fs"); 
+
+// DEBUT DE LA FONCTION
+
+var trait = function (req, res, query) {
+
+	var cartes;
+	var river = [0, 0, 0];
+	var i;
+	var j;
+	
+	// DISTRIBUTION DES CARTES DANS RIVIERE
+
+	for (i = 0 ; i =<3 ; i++) {
+		river = cartes[i];
+	}
+
+	// DISTRIBUTION DES CARTES AUX JOUEURS
+
+	for (i = 0 ; i < membres.length ; i++) {
+		for (j = O ; i < 2 ; j++) {
+			carteJoueur[i]
+
+
+
+
+
+
+
+
+
+
+
+};
+//--------------------------------------------------------------------------
+
+module.exports = trait;
