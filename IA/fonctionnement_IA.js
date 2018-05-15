877204a80dc128a58df0f8dafa6db0db866649f7
Benjamin Rodriguez <rodriguez@intechinfo;fr>
2018-05-15 14:36:29 +0200

    modif ia

--- IA/fonctionnement_IA.js
+++ IA/fonctionnement_IA.js
@@ -1,5 +1,5 @@
 //=========================================================================
-// Traitement de "focntionneme_IA.js"
+// Traitement de "focntionnemnent_IA.js"
 // Auteur : ALL IN'TECH 
 // Version : 01/05/2018
 //=========================================================================
@@ -8,26 +8,30 @@
 // EN TETE
 
 var fs = require ("fs");
-var mise;
 
-// CORPS
+var fonctionnement_IA = function (x, y, z) {
+	
+	var mise;
 
-// FONCTION DE COMBINAISONS
+	// CORPS
 
-var = require ("../fonctions/function_brelan.js");
-var = require ("../fonctions/function_carre.js");
-var = require ("../fonctions/function_carte_haute.js");
-var = require ("../fonctions/function_couleur.js");
-var = require ("../fonctions/function_double_paire.js");
-var = require ("../fonctions/function_egalite.js");
-var = require ("../fonctions/function_full.js");
-var = require ("../fonctions/function_paire.js");
-var = require ("../fonctions/function_quinte.js");
-var = require ("../fonctions/function_quinte_flush.js");
-var = require ("../fonctions/function_quinte_flush_royale.js");
+	// FONCTION DE COMBINAISONS
 
-// MISE DE L'IA
+	var = require ("../fonctions/function_brelan.js");
+	var = require ("../fonctions/function_carre.js");
+	var = require ("../fonctions/function_carte_haute.js");
+	var = require ("../fonctions/function_couleur.js");
+	var = require ("../fonctions/function_double_paire.js");
+	var = require ("../fonctions/function_egalite.js");
+	var = require ("../fonctions/function_full.js");
+	var = require ("../fonctions/function_paire.js");
+	var = require ("../fonctions/function_quinte.js");
+	var = require ("../fonctions/function_quinte_flush.js");
+	var = require ("../fonctions/function_quinte_flush_royale.js");
 
-var = require ("./mise_IA.js");
+	// MISE DE L'IA
 
-mise = mise_IA(req, res, query);
+	var = require ("./mise_IA.js");
+
+	mise = mise_IA(req, res, query);
+};
