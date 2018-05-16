cc24796f852aba012b36c625f9e15c5e7568f749
Benjamin Rodriguez <rodriguez@intechinfo;fr>
2018-05-16 15:43:55 +0200

    ajout miser checker suivre relancer coucher modif ajout de page joueur avec option de jeu

--- pi.js
+++ pi.js
@@ -38,9 +38,13 @@ var req_attendre_tour = require ("./req/req_attendre_tour.js");
 var req_jouer = require ("./req/req_jouer.js");
 var req_creerTable = require ("./req/req_creerTable.js");
 var req_attendre_joueur = require ("./req/req_attendre_joueur.js");
-var req_creerTable = require ("./req/req_creerTable.js")
-var req_req_attendre_partie = require ("./req/req_attendre_partie.js");
-
+var req_creerTable = require ("./req/req_creerTable.js");
+var req_attendre_partie = require ("./req/req_attendre_partie.js");
+var req_miser = require ("./req/req_miser.js");	
+var req_relancer = require ("./req/req_relancer.js"); 
+var req_checker = require ("./req/req_checker.js"); 
+var req_suivre = require ("./req/req_suivre.js"); 
+var req_coucher = require ("./req/req_coucher.js"); 
 
 var req_erreur = require("./req/req_erreur.js");
 
@@ -88,9 +92,6 @@ var traite_requete = function (req, res) {
 			case '/req_quitter_table_rejointe':
 				req_quitter_table_rejointe(req, res, query);
 				break;
-//			case '/req_modele_page_table':
-//				req_modele_page_table(req, res, query);
-//				break;
 			case '/req_modele_page_joueur':
 				req_modele_page_joueur(req, res, query);
 				break;
@@ -139,6 +140,22 @@ var traite_requete = function (req, res) {
 			case '/req_attendre_partie':
 				req_attendre_partie (req, res, query);
 				break;
+			case '/req_miser':
+				req_miser (req, res, query);
+				break;
+			 case '/req_checker':
+                req_checker (req, res, query);
+                break;
+			 case '/req_relancer':
+                req_relancer (req, res, query);
+                break;
+			case '/req_suivre':
+                req_suivre (req, res, query);
+                break;
+			case '/req_coucher':
+                req_coucher (req, res, query);
+                break;
+
             default:
                 req_statique(req, res, pathname);
                 break;
