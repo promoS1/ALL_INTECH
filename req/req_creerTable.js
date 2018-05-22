58a27890ca683672e9ac72b7b268a7442f451712
nahel <lachgar@intechinfo.fr>
2018-05-22 10:39:03 +0200

    json

--- req/req_creerTable.js
+++ req/req_creerTable.js
@@ -15,23 +15,36 @@ var trait = function (req, res, query) {
 	var partie;
 	var contenuPartie;
 	var joueurs;
-
+	var hotes = {};
+	var contenuHotes;
+	var riviere;
+	var contenuRiviere;
     // AFFICHAGE DE LA PAGE D'ACCUEIL
 
 
 	// DONNE INFOS DE L'ADMIN 
 
-	joueurs = {};
+	joueurs = [];
 	joueurs.compte = query.compte;
 	joueurs.debute = "true";
 	joueurs.position = "";
 	joueurs.cartes = "";
 	joueurs.solde = "";
 
+	contenuHotes = fs.readFileSync("./tables/hotes.json", "UTF-8");
+	hotes = JSON.parse(contenuHotes);
+
+	hotes.push(query.compte);
+
+	contenuHotes = JSON.stringify(hotes);
+	fs.writeFileSync("./tables/hotes.json", contenuHotes, "UTF-8");
 
 
 	contenuPartie = JSON.stringify(joueurs);
-	fs.writeFileSync("./tables/" + query.compte +".json", contenuPartie, "UTF-8");
+	fs.writeFileSync("./tables/" + query.compte + ".json", contenuPartie, "UTF-8");
+
+	contenuRiviere = JSON.stringify(riviere);
+	fs.writeFileSync("./tables/" + query.compte + " Riviere.json", contenuRiviere);
 
     page = fs.readFileSync('./html/modele_page_table.html' , 'utf-8');
 
