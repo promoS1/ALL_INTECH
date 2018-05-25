e652310783ba11d2c1357e6d24dfab842e279754
nahel <lachgar@intechinfo.fr>
2018-05-25 19:59:14 +0200

    a

---
+++ test/req/req_modele_page_joueur.js
@@ -0,0 +1,71 @@
+//=========================================================================
+// Traitement de "req_modele_page_joueur"
+// Auteur :ALL IN'TECH 
+// Version : 25/04/2018
+//=========================================================================
+"use strict";
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function (req, res, query) {
+
+    var marqueurs;
+    var page;
+	var joueurs;
+	var contenuPartie;
+	var contenu_fichier;
+	var connecte;
+	var debut;
+
+    // AFFICHAGE DE LA PAGE D'ACCUEIL
+
+    page = fs.readFileSync('./html/modele_page_joueur.html', 'utf-8');
+
+
+	joueurs = {};
+    joueurs.compte = query.compte;
+    joueurs.position = "";
+    joueurs.cartes = "";
+    joueurs.solde = "100";
+
+
+// PARTIE DEBUTE -> ECRITURE DANS JSON
+	contenu_fichier = fs.readFileSync ('./json/connecte.json' , 'utf-8');
+    connecte = JSON.parse(contenu_fichier);
+
+
+	for (var i = 0 ; i < connecte.length ; i++) {
+		if (query.compte === connecte[i].compte) {
+			connecte[i].table = query.table;
+		}
+	}
+
+    contenu_fichier = JSON.stringify (connecte);
+    fs.writeFileSync ('./json/connecte.json' , contenu_fichier , 'utf-8');
+
+
+
+	// DISTRIBUTION DES CARTES
+	
+    contenuPartie = JSON.stringify(joueurs);
+    fs.writeFileSync("./tables/" + query.compte +".json", contenuPartie, "UTF-8");
+
+
+    marqueurs = {};
+    marqueurs.compte = query.compte;
+	marqueurs.table = query.table;
+    page = page.supplant(marqueurs);
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(page);
+    res.end();
+
+
+
+
+};
+//--------------------------------------------------------------------------
+
+module.exports = trait;
+
