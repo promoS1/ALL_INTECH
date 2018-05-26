d43f7c00c553aff45948afc81c5c8949bd33616b
ELEVE <ELEVE@intechS1.fr>
2018-05-26 13:10:43 +0200

    re

--- test/req/req_modele_table_rejointe.js
+++ test/req/req_modele_table_rejointe.js
@@ -6,7 +6,7 @@
 "use strict";
 
 var fs = require("fs");
-require('remedial');
+var remedial = require('remedial');
 
 var trait = function (req, res, query) {
 
@@ -15,6 +15,9 @@ var trait = function (req, res, query) {
 	var contenu;
 	var joueurs;
 	var connecte;
+	var table;
+	var adversaire;
+	var nouvellePartie;
 	var i;
 
 // ON LIT LE FICHIER DES JOUEURS CONNECTÉS
@@ -23,14 +26,23 @@ var trait = function (req, res, query) {
 	
 // ON REGARDE SI L'ADMIN A LANCÉ LA PARTIE
 	connecte = {};
-	
-for (i = 0; i < connecte.length; i++) {
-	if (connecte[i].table === "") {
-		page = fs.readFileSync ('./html/modele_page_adversaire.html');
-	} else {
-		page = fs.readFileSync ('./html/modele_table_rejointe.html');
+		
+	for (i = 0; i < connecte.length; i++) {
+		if (connecte[i].table === "") {
+			page = fs.readFileSync ('./html/modele_page_adversaire.html');
+		} else {
+			page = fs.readFileSync ('./html/modele_table_rejointe.html');
+		}
 	}
-}
+
+
+	contenu = fs.readFileSync ("./tables/a.json" , "utf-8");
+	table = JSON.parse(contenu);
+	 
+
+	
+
+
 
 // AFFICHAGE DE LA PAGE D'ACCUEIL
     page = fs.readFileSync('./html/modele_table_rejointe.html', 'utf-8');
@@ -38,6 +50,7 @@ for (i = 0; i < connecte.length; i++) {
 	marqueurs = {};
 	marqueurs.compte = query.compte;
 	marqueurs.table = query.table;
+	marqueurs.adversaire = adversaire;
 	page = page.supplant(marqueurs);
 
     res.writeHead(200, {'Content-Type': 'text/html'});
