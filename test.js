83038975b7ab95a92e5b4be14763feaa041b5d8b
BARAODASILVA <BARAODASILVA@intechS1.fr>
2018-05-27 17:28:18 +0200

    Ajout des modules Distribution et melange dans creerTable.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+"use strict";
 //========================================================================
 // Traitement de "req_creerTable"
 // Auteur : ALL'INTECH 
@@ -8,8 +9,6 @@
 var fs = require("fs");
 var remedial = require('remedial');
 
-var trait = function (req, res, query) {
-
     var marqueurs;
     var page;
 	var partie = [];
@@ -18,8 +17,8 @@ var trait = function (req, res, query) {
 	var connecte;
 	var i;
 	var nouvellePartie;
-	var distribution;
 	var melange;
+	var distribution;
 	var mains;
 	var river;
 
@@ -37,7 +36,7 @@ var trait = function (req, res, query) {
 
 	contenu_partie = JSON.stringify(partie);
 	fs.writeFileSync("./tables/"+query.compte+".json", contenu_partie,  "UTF-8");
-*/
+
 	// LANCEMENT PARTIE EN ATTENTE -> TRUE
 	contenu_fichier = fs.readFileSync ('./json/connecte.json' , 'utf-8');
 	connecte = JSON.parse(contenu_fichier);
@@ -50,9 +49,9 @@ var trait = function (req, res, query) {
 	
 	contenu_fichier = JSON.stringify (connecte);
 	fs.writeFileSync ('./json/connecte.json' , contenu_fichier , 'utf-8');
-	
+*/	
 	// APPEL DU MODULE
-
+	
 	melange = require("./fonctions/function_melange_cartes.js");
 	distribution = require("./fonctions/function_distribution_cartes.js");
 
@@ -61,20 +60,40 @@ var trait = function (req, res, query) {
 	melange();
 
 	nouvellePartie = {};
-	nouvellePartie.admin = query.compte;
+	nouvellePartie.admin = "Nahel";
 	nouvellePartie.en_jeu = false;
 	
 	nouvellePartie.joueurs = [];
-	nouvellePartie.joueurs[0] = query.compte;
+	nouvellePartie.joueurs[0] = "Nahel";
 	//nouvellePartie.joueurs[1] = "titi";
 	//nouvellePartie.joueurs[2] = "toto"
 
 	nouvellePartie.river = [];
+
+	/*
+	nouvellePartie.river[0] = {"valeur" : 0 , "couleur" : "k"};
+	nouvellePartie.river[1] = {"valeur" : 0 , "couleur" : "p"};
+	nouvellePartie.river[2] = {"valeur" : 0 , "couleur" : "c"};
+	nouvellePartie.river[3] = {"valeur" : 0 , "couleur" : "t"};
+	nouvellePartie.river[4] = {"valeur" : 0 , "couleur" : "k"};
+	*/
 	
 	nouvellePartie.main = [];
 	nouvellePartie.main[0] = [];
 	nouvellePartie.main[1] = [];
-	
+	/*
+	nouvellePartie.main[0] = {"valeur" : 0 , "couleur" : "p"};
+	nouvellePartie.main[0] = {"valeur" : 0 , "couleur" : "k"}; 
+
+	nouvellePartie.main[1] = [];
+	nouvellePartie.main[1][0] = {"valeur" : 0 ,"couleur" : "p"};
+	nouvellePartie.main[1][1] = {"valeur" : 0 ,"couleur" : "t"};
+
+	nouvellePartie.main[2] = [];
+	nouvellePartie.main[2][0] = {"valeur" : 0 , "couleur": "t"};
+	nouvellePartie.main[2][1] = {"valeur" : 0 , "couleur" : "k"};
+	*/
+
 	nouvellePartie.mise = [];
 	nouvellePartie.mise[0] = 0 ;
 	nouvellePartie.mise[1] = 0 ;
@@ -94,26 +113,12 @@ var trait = function (req, res, query) {
 
 	distribution(mains, river);
 
-	//listeConnecte[listeConnecte.length] = nouvellePartie;
-
 	contenu_partie = JSON.stringify(nouvellePartie);
 
-	fs.writeFileSync("./tables/"+query.compte+".json", contenu_partie, "UTF-8");
+	fs.writeFileSync("./tables/nahel.json", contenu_partie, "UTF-8");
 
 
-	// AFFICHAGE DE LA PAGE
 
-    page = fs.readFileSync("./html/modele_page_joueur.html" , "UTF-8");
-
-    marqueurs = {};
-    marqueurs.compte = query.compte;
-	marqueurs.tables = query.compte;
-    page = page.supplant(marqueurs);
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(page);
-    res.end();
-};
 //--------------------------------------------------------------------------
 
-module.exports = trait;
+
