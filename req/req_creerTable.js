cf4cb460f55fcfa78197a787887d928b4bbbc552
Benjamin RODRIGUEZ <rodriguez@intechinfo.fr>
2018-06-20 10:53:16 +0200

    biuiu

--- req/req_creerTable.js
+++ req/req_creerTable.js
@@ -1,10 +1,11 @@
-//=============================================================================
+//==============================================================================
 // Traitement de "req_creerTable"
 // Auteur : ALL IN'TECH 
-// Version : 17/06/18
+// Version : 20/06/18
 //==============================================================================
 "use strict";
 
+// ================================= EN TETE ===================================
 var fs = require("fs");
 var remedial = require("remedial");
 
@@ -37,7 +38,6 @@ var trait = function (req, res, query) {
 	var soldeAdversaire;
 	var miseJoueur;
 	var miseAdversaire;
-	var choix;
 	var attendre;
 	var pot;
 	var coucher;
@@ -51,23 +51,25 @@ var trait = function (req, res, query) {
 	var couleur2Joueur;
 	var riviere;
 
-	// VARIABLE QUI APPELLE LA FONCTION
+	// VARIABLES QUI APPELLE LA FONCTION
 	var carteHaute = require("../fonctions/function_carte_haute.js");
 	var paire = require("../fonctions/function_paire.js");
 	var doublePaire = require("../fonctions/function_double_paire.js");
 	var brelan = require("../fonctions/function_brelan.js");
 	var quinte = require("../fonctions/function_quinte.js");
-//	var couleur = require("../fonctions/function_couleur.js");
+	var couleur = require("../fonctions/function_couleur.js");
 	var full = require("../fonctions/function_full.js");
 	var carre = require("../fonctions/function_carre.js");
 	var quinteFlush = require("../fonctions/function_quinte_flush.js");
 	var quinteFlushRoyale = require("../fonctions/function_quinte_flush_royale.js");
 
+//=================================== CORPS ====================================
+
 	// LECTURE DU JSON DES MEMBRES
 	contenu_fichier = fs.readFileSync("./json/connecte.json", "UTF-8");
 	membres = JSON.parse(contenu_fichier);
 
-	// ON DONNE LA DISPONIBILITE DE CHAQUE JOUEURS
+	// DISPONIBILITE DE CHAQUE JOUEURS
 	for (a = 0 ; a < membres.length ; a++) {
 		if (membres[a].compte === query.compte) {
 			membres[a].connecte = "joue";
@@ -79,11 +81,10 @@ var trait = function (req, res, query) {
 		}
 	}
 
-	// LANCEMENT DU TOUR
 	contenu_fichier = JSON.stringify(membres);
 	fs.writeFileSync("./json/connecte.json" , contenu_fichier , "UTF-8");
 
-	// CODE JEU
+	// CODE DU JEU
 
 	// APPEL DES MODULES MELANGER ET LES COMBINAISONS
 	melange = require("../fonctions/function_melange_cartes.js");
@@ -92,7 +93,6 @@ var trait = function (req, res, query) {
 	// APPEL DES FONCTIONS
 	melange();
 
-	// ECRITURE DU JSON DE PARTIE
 	nouvellePartie = {};
 	nouvellePartie.admin = query.compte;
 
@@ -117,34 +117,30 @@ var trait = function (req, res, query) {
 	nouvellePartie.mise[0] = 0;
 	nouvellePartie.mise[1] = 0;
 
-	//COUCHER
 	nouvellePartie.coucher = false;
 	
-	//PHASE
 	nouvellePartie.phase = 0;
 
-	// POT
-	//nouvellePartie.pot = Number(nouvellePartie.pot);
 	nouvellePartie.pot = Number(0);
 
 	for (i = 0 ; i < nouvellePartie.mise[i] ; i++) {
 		nouvellePartie.solde += nouvellePartie.mise[i];
 	}
-	// SOLDE DE CHAQUE JOUEURS, SOLDE DE DEPART DE 100
+	// SOLDE DE CHAQUE JOUEUR
 	nouvellePartie.solde = [];
-	nouvellePartie.solde[0] = 1000;
-	nouvellePartie.solde[1] = 1000;
+	nouvellePartie.solde[0] = 10000;
+	nouvellePartie.solde[1] = 10000;
 
 	// DISTRIBUTION DES CARTES DANS LA MAIN ET DANS LA RIVIERE
 	mains = nouvellePartie.main;
 	river = nouvellePartie.river;
 
-	// ATTENDRE TRUE OU FALSE
 	nouvellePartie.attendre = [false,false];
 
 	// VALEUR DES MAINS
 	nouvellePartie.valeurMain = [0,0];
 
+	// ON DISTRIBUE LES MAINS ET LA RIVIERE
 	distribution(mains, river);
 
 	// ECRITURE DANS LE JSON DE PARTIE AVEC LES NOUVELLES DONNEES
@@ -156,7 +152,7 @@ var trait = function (req, res, query) {
 	nouvellePartie = JSON.parse(contenu_partie);
 
 	// JOUEURS 1
-	if(query.compte === nouvellePartie.joueurs[0]){
+	if (query.compte === nouvellePartie.joueurs[0]) {
 		carteJoueurs = nouvellePartie.main[0][0].couleur + nouvellePartie.main[0][0].valeur;
 		carte2Joueurs = nouvellePartie.main[0][1].couleur + nouvellePartie.main[0][1].valeur;
 		miseJoueur = nouvellePartie.mise[0];
@@ -166,7 +162,7 @@ var trait = function (req, res, query) {
 	}
 
 	// JOUEUR 2
-	if(query.compte === nouvellePartie.joueurs[1]){
+	if (query.compte === nouvellePartie.joueurs[1]) {
 		carteJoueurs = nouvellePartie.main[0][2].couleur + nouvellePartie.main[0][2].valeur;
 		carte2Joueurs = nouvellePartie.main[0][3].couleur + nouvellePartie.main[0][3].valeur;
 		miseJoueur = nouvellePartie.mise[1];
@@ -176,7 +172,6 @@ var trait = function (req, res, query) {
 	}
 
 	pot = nouvellePartie.pot;
-
 
 	carte1Riviere = "<img class='cartes' src='../img/carte_verso_2.png'>"; 
 	carte2Riviere = "<img class='cartes' src='../img/carte_verso_2.png'>"; 
@@ -196,8 +191,7 @@ var trait = function (req, res, query) {
 	partie = query.compte;
 	riviere = nouvellePartie.river;
 
-	for(x = 0; x < nouvellePartie.joueurs.length; x++){
-
+	for (x = 0 ; x < nouvellePartie.joueurs.length ; x++) {
 		carte1Joueur = nouvellePartie.main[x][0].valeur;
 		carte2Joueur = nouvellePartie.main[x][1].valeur;
 		couleur1Joueur = nouvellePartie.main[x][0].couleur;
@@ -208,12 +202,11 @@ var trait = function (req, res, query) {
 		doublePaire(carte1Joueur, carte2Joueur, riviere, x, partie);
 		brelan(carte1Joueur, carte2Joueur, riviere, x, partie);
 		quinte(carte1Joueur, carte2Joueur, riviere, x, partie);
-//		couleur(couleur1Joueur, couleur2Joueur, riviere, x, partie);
+		//couleur(couleur1Joueur, couleur2Joueur, riviere, x, partie);
 		full(carte1Joueur, carte2Joueur, riviere, x, partie);
 		carre(carte1Joueur, carte2Joueur, riviere, x, partie);
 		quinteFlush(carte1Joueur, carte2Joueur, riviere, x, partie);
 		//quinteFlushRoyale(carte1Joueur, carte2Joueur, riviere, x, partie);
-
 	}
 
 	// AFFICHAGE DE LA PAGE DE JEU
@@ -222,11 +215,9 @@ var trait = function (req, res, query) {
 	// MARQUEURS HTML
 	marqueurs = {};
 
-	// MARQUEURS CARTE JOUEUR
 	marqueurs.carte2Joueurs = carte2Joueurs;
 	marqueurs.carteJoueurs = carteJoueurs;
 
-	// MARQUEURS CARTE DE LA RIVIERE
 	marqueurs.carte1Riviere = carte1Riviere;
 	marqueurs.carte2Riviere = carte2Riviere;
 	marqueurs.carte3Riviere = carte3Riviere;
@@ -238,7 +229,6 @@ var trait = function (req, res, query) {
 	marqueurs.pot = pot;
 	marqueurs.miseJoueur = miseJoueur;
 	marqueurs.miseAdversaire = miseAdversaire;
-	marqueurs.choix = choix;
 
 	marqueurs.compte = query.compte;
 	marqueurs.adversaire = query.adversaire;
