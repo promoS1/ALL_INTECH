b020df9d82d799857e45df8b1ee310e59c600cb3
Benjamin RODRIGUEZ <rodriguez@intechinfo.fr>
2018-06-23 18:55:31 +0200

    bibui

--- pi.js
+++ pi.js
@@ -36,7 +36,7 @@ var req_attendre_fin = require ("./req/req_attendre_fin.js");
 var req_refuser_defi = require ("./req/req_refuser_defi.js");
 var req_regles = require ("./req/req_regles.js");
 var req_salon_visiteurs = require ("./req/req_salon_visiteurs.js");
-var req_creer_table_IA = require ("./IA/req_creer_table_IA.js");
+//var req_creer_table_IA = require ("./IA/req_creer_table_IA.js");
 var req_allin = require ("./req/req_allin.js");
 
 var req_erreur = require("./req/req_erreur.js");
@@ -122,9 +122,9 @@ var traite_requete = function (req, res) {
 			case '/req_salon_visiteurs':
 				req_salon_visiteurs (req, res, query);
 				break;
-			case '/req_creer_table_IA':
-				req_creer_table_IA (req, res, query);
-				break;
+			//case '/req_creer_table_IA':
+			//	req_creer_table_IA (req, res, query);
+			//	break;
 			case '/req_allin':
 				req_allin (req, res, query);
 				break;
